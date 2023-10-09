422c41ae5ca11e58b799b911401690a5154d785c
Uj66 <umairjafar66@gmail.com>
2023-10-09 04:23:23 +0100

    Clearing the example files of playwright

---
+++ testConfig.ts
@@ -0,0 +1,9 @@
+export const testConfig = {
+    testurl: 'https://qa-5.monevo.com/',
+    firstName: `Umair`,
+    lastName: `Jafar`,
+    emailAddress: `test@test.com`,
+    dateOfBirth: `24/12/1991`,
+    correctPhoneNumber: `07897641544`,
+    incorrectPhoneNumber: `310-323-258`
+}
